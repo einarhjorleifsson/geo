f31cb9fbe48fab0071c59324bb4b6cc17a057c2b
Sigurður Þór Jónsson <sigurdur.thor.jonsson@gmail.com>
2018-04-12 08:35:23 +0000

    add NULL instead of break

--- R/pointkriging.R
+++ R/pointkriging.R
@@ -144,7 +144,7 @@ function(lat, lon, z, xgr, vagram, maxnumber = 16, scale = "km", option = 1,
 	if(length(areas) > 1) {	
 	ind <- c(1:length(areas$lat))
 		ind <- ind[is.na(areas$lat)]
-		if(length(ind) == 0)
+		if(length(ind) == 0) NULL
 #			break()
 		nareas <- length(ind) + 1
 		#number of areas
